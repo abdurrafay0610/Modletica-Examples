e31b429ea66d529134c73b80294e703bb83b1d60
abdurrafay0610 <abdurrafay.0610@gmail.com>
2025-08-02 15:31:10 +0500

    Simple for loop example

---
+++ Loops/For Loops/For Loop 1.c
@@ -0,0 +1,13 @@
+
+
+/*
+Function Description: This is a very simple for loop. It will run a very generic for loop. Th purpose is to show the mapping of for made by modletica.
+*/ 
+
+void for_loop_1()
+{
+	for(int i=0; i<10;i++)
+	{
+		// For body here
+	}
+}
